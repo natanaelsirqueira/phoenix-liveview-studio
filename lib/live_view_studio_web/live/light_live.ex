8879ee85f413a6a3c2019da96f39ba27ff2b357e
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-06-23 23:12:08 -0400

    Exercise: Light Me Up!

--- lib/live_view_studio_web/live/light_live.ex
+++ lib/live_view_studio_web/live/light_live.ex
@@ -3,12 +3,15 @@ defmodule LiveViewStudioWeb.LightLive do
 
   def mount(_params, _session, socket) do
     socket = assign(socket, :brightness, 10)
+
     {:ok, socket}
   end
 
+  @spec render(any) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~L"""
     <h1>Front Porch Light</h1>
+
     <div id="light">
       <div class="meter">
         <span style="width: <%= @brightness %>%">
@@ -37,21 +40,25 @@ defmodule LiveViewStudioWeb.LightLive do
 
   def handle_event("on", _, socket) do
     socket = assign(socket, :brightness, 100)
+
     {:noreply, socket}
   end
 
   def handle_event("up", _, socket) do
-    socket = update(socket, :brightness, &(&1 + 10))
+    socket = update(socket, :brightness, &min(&1 + 10, 100))
+
     {:noreply, socket}
   end
 
   def handle_event("down", _, socket) do
-    socket = update(socket, :brightness, &(&1 - 10))
+    socket = update(socket, :brightness, &max(&1 - 10, 0))
+
     {:noreply, socket}
   end
 
   def handle_event("off", _, socket) do
     socket = assign(socket, :brightness, 0)
+
     {:noreply, socket}
   end
 end
